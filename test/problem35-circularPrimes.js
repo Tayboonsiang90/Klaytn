61b93af38838ec75955eb99d7b79202cba0fdf91
Boon <tayboonsiang90@gmail.com>
2022-06-19 00:37:21 +0800

    Updated solutions and tests

--- test/problem35-circularPrimes.js
+++ test/problem35-circularPrimes.js
@@ -3,10 +3,13 @@ const circularPrimes = require("../app/problem35-circularPrimes");
 const chai = require("chai");
 const expect = chai.expect;
 
-// describe("problem35-circularPrimes.js", () => {
-//     context("circularPrimes", () => {
-//         it("Should add two numbers", () => {
-//             expect(circularPrimes.main()).to.equal(55);
-//         });
-//     });
-// });
+describe("problem35-circularPrimes.js", () => {
+    context("countCircularPrimes(n)", () => {
+        it("Should return the number of circular primes below 100, n=100", () => {
+            expect(circularPrimes.countCircularPrimes(100)).to.equal(13);
+        });
+        it("Should return the number of circular primes below one million, n=1000000", () => {
+            expect(circularPrimes.countCircularPrimes(1000000)).to.equal(55);
+        });
+    });
+});
